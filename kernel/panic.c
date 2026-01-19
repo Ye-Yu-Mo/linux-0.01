674c7ff194f0024dff0bd6db7ec0d30260078f41
Ye-Yu-Mo <2908156919@qq.com>
2026-01-19 13:41:19 +0800

    feat: format

--- kernel/panic.c
+++ kernel/panic.c
@@ -4,8 +4,8 @@
  */
 #include <linux/kernel.h>
 
-volatile void panic(const char * s)
-{
-	printk("Kernel panic: %s\n\r",s);
-	for(;;);
+volatile void panic(const char *s) {
+  printk("Kernel panic: %s\n\r", s);
+  for (;;)
+    ;
 }
